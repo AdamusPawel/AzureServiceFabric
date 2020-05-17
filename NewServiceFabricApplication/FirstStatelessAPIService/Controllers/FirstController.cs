5712e41e123ea0a169586732e37913852280bd23
AdamusPawel <adamuspawel95@gmail.com>
2020-05-17 21:30:31 +0200

    Stateless API Service demo

---
+++ NewServiceFabricApplication/FirstStatelessAPIService/Controllers/FirstController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FirstStatelessAPIService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FirstController : ControllerBase
+    {
+        [HttpGet]
+        [Route("get")]
+        public async Task Get() // method when that API endpoint will be called
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
