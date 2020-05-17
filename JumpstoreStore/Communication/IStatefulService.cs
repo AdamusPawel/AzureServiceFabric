37ed3209eb15f939ff2b57eb9c528e86219a96f2
AdamusPawel <adamuspawel95@gmail.com>
2020-05-18 01:48:17 +0200

    Setting up communication between services

---
+++ JumpstoreStore/Communication/IStatefulService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.ServiceFabric.Services.Remoting;
+using System;
+using System.Threading.Tasks;
+
+namespace Communication
+{
+    public interface IStatefulService : IService
+    {
+        Task<string> GetServiceDetails();
+    }
+}
